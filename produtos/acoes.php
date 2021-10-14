eeee7691723d84b9e0a2abac00190acec1d99f47
GUILHERME LEDIER ALVES <21192997@CFP127ALUNO.LOCAL>
2021-10-14 09:56:55 -0300

    14/10 - correção imagens

--- produtos/acoes.php
+++ produtos/acoes.php
@@ -1,35 +1,28 @@
-<?php 
+<?php
 
     require("../database/conexao.php");
 
     switch ($_POST["acao"]) {
 
         case 'inserir':
-
-            /* TRATAMENTO DA IMAGEM PARA UPLOAD */ 
-
-            // echo '<pre>';
-            // var_dump($_FILES);
-            // echo '<pre>';
-
-            // RECUPERA O NOME DP ARQUIVO
-            $nomeArquivo = $_FILES["foto"]["name"];
-
-            // RECUPERAR A EXTENSÃO DO ARQUIVO
-            $extensao = pathinfo($nomeArquivo, PATHINFO_EXTENSION);
- 
-            // DEFINIR UM NOVO NOME PARA O ARQUIVO DE IMAGEM
-            $novoNome = md5(microtime() . "." . $extensao);
-
-            // echo $nomeArquivo;
-            // echo '<br>';
-            // echo $novoNome;
             
-            // UPLOAD DO ARQUIVO
+            /* TRATAMENTO DA IMAGEM PARA UPLOAD: */
+
+            //RECUPERA O NOME DO ARQUIVO
+            $nomeArquivo = $_FILES["foto"]["name"];
+            
+            //RECUPERAR A EXTENSÃO DO ARQUIVO
+            $extensao = pathinfo($nomeArquivo, PATHINFO_EXTENSION);
+
+            //DEFINIR UM NOVO NOME PARA O ARQUIVO DE IMAGEM
+            $novoNome = md5(microtime()) . "." . $extensao;
+
+            //UPLOAD DO ARQUIVO:
             move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/$novoNome");
 
+            /* INSERÇÃO DE DADOS NA BASE DE DADOS DO MYSQL: */
 
-            // RECEBENDO DADOS 
+            //RECEBEIMENTO DOS DADOS:
             $descricao = $_POST["descricao"];
             $peso = $_POST["peso"];
             $quantidade = $_POST["quantidade"];
@@ -39,26 +32,23 @@
             $desconto = $_POST["desconto"];
             $categoriaId = $_POST["categoria"];
 
-            // CRIAÇÃO DA INSTRUÇÃO SQL DE INSERÇÃO
-            $sql = "INSERT INTO tbl_produto
-            (descricao, peso, quantidade, cor, tamanho, valor, desconto, imagem, categoria_id)
-            VALUES ('$descricao', $peso, $quantidade, '$cor','$tamanho', $valor, 
-           $desconto, '$nomeArquivo', $categoriaId)";
+            //CRIAÇÃO DA INSTRUÇÃO SQL DE INSERÇÃO:
+            $sql = "INSERT INTO tbl_produto 
+            (descricao, peso, quantidade, cor, tamanho, valor, desconto, imagem, categoria_id) 
+            VALUES ('$descricao', $peso, $quantidade, '$cor', '$tamanho', $valor, $desconto, 
+            '$novoNome', $categoriaId)";
 
-
-            // EXECUÇÃO DO SQL DE INSERÇÃO
+            //EXCUÇÃO DO SQL DE INSERÇÃO:
             $resultado = mysqli_query($conexao, $sql);
 
-            // REDIRECIONAR PARA INDEX
+            //REDIRECIONAR PARA INDEX:
             header('location: index.php');
 
             break;
-
-
+        
         default:
             # code...
             break;
     }
-
 
 ?>
